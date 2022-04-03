7a55af7c929468ab0c1a684d4ad8e41823ca16be
tahirata1983 <tahiratayev@yahoo.com>
2022-04-03 14:37:06 +0300

    Initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,12 @@
+public class Test1 {
+    public static void main(String[] args) {
+
+
+        String str="tahirata";
+        if(str.contains("A")){
+            System.out.println("ok...");
+        }else{
+            System.out.println("not ok...");
+        }
+    }
+}
