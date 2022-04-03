446234aed0b6edb4b11fcb8f23b5cd25f464665c
atatahir <tahiratayev@yahoo.com>
2022-04-03 04:44:16 -0700

    it is my first commit

---
+++ src/test/java/Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+    public static void main(String[] args) {
+
+
+        String str="tahirata";
+        if(str.contains("B")){
+            System.out.println("ok...");
+        }else{
+            System.out.println("not ok...");
+        }
+    }
+}
